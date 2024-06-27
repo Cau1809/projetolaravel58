5618826f509f668237276d78ad80c9f482df85b7
Cauani <cauani@Macbook>
2024-06-27 19:16:51 -0300

    Adicionado model e migração para candidatos

---
+++ database/migrations/2024_06_27_220112_create_candidatos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCandidatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidatos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->integer('idade');
+            $table->string('sexo');
+            $table->string('escolaridade');
+            $table->enum('cor_favorita', ['azul', 'branco', 'vermelho']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidatos');
+    }
+}
